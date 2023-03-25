7963a8f56ac5bf77bf4ec3288a442494d3bde050
attlog <97865180+BoredPositron@users.noreply.github.com>
2023-03-25 22:13:12 +0530

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+function checkAnswers() {
+  var honeymoon = document.getElementById("honeymoon").value.toLowerCase();
+  var kids = document.getElementById("kids").value.toLowerCase();
+  var home = document.getElementById("home").value.toLowerCase();
+  var wedding = document.getElementById("wedding").value.toLowerCase();
+  var proposal = document.getElementById("proposal").value.toLowerCase();
+  var together = document.getElementById("together").value.toLowerCase();
+  alert("You very cute mwah")
+  window.location = "letter.html"
+}
